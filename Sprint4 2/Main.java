73d650b827ad5c99b8a0867b2514e5b6bb813b9e
abdulkiad <abdulrahman.salim@ucdconnect.ie>
2018-04-13 12:48:56 +0100

    Chris's Game  to help with our monopoly

---
+++ Sprint4 2/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main (String args[]) {	
+		Monopoly monopoly = new Monopoly();		
+		monopoly.inputNames();
+		monopoly.giveStartMoney();
+		monopoly.decideStarter();
+		do {
+			monopoly.processTurn();
+			if (!monopoly.isGameOver()) {
+				monopoly.nextPlayer();
+			}
+		} while (!monopoly.isGameOver());
+		monopoly.decideWinner();
+		monopoly.displayGameOver();
+		return;
+	}
+	
+}
