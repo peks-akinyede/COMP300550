745650b7a55ee629537feff2eb1ff560f7ce96da
abdulkiad <abdulrahman.salim@ucdconnect.ie>
2018-04-15 19:41:47 +0100

    game is run here

--- tonyInterfaces/Main.java
+++ tonyInterfaces/Main.java
@@ -52,3 +52,21 @@
 //
 //
 //}
+
+// public class Main {
+// 	public static void main (String args[]) {	
+// 		Monopoly monopoly = new Monopoly();		
+// 		monopoly.inputNames();
+// 		monopoly.giveStartMoney();
+// 		monopoly.decideStarter();
+// 		do {
+// 			monopoly.processTurn();
+// 			if (!monopoly.isGameOver()) {
+// 				monopoly.nextPlayer();
+// 			}
+// 		} while (!monopoly.isGameOver());
+// 		monopoly.decideWinner();
+// 		monopoly.displayGameOver();
+// 		return;
+// 	}
+// }
