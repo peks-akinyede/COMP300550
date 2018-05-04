3fac7836bc4cab9aeb34e02f68651ba452329aa7
barney <robert.barnes@ucdconnect.ie>
2018-05-04 18:19:15 +0100

    Update GenerateCards.java

--- tonyInterfaces/GenerateCards.java
+++ tonyInterfaces/GenerateCards.java
@@ -50,39 +50,14 @@ public class GenerateCards {
 		knowledgeDir = "C:\\Users\\robert\\Documents\\.Year 3_Part2\\software eng3\\noc\\TSV Lists\\";
 		
 		NOC           = new KnowledgeBaseModule(knowledgeDir + "Veale's The NOC List.txt", 0);
-		//	CATEGORIES    = new KnowledgeBaseModule(knowledgeDir + "Category Hierarchy.txt", 0);
+		
 	//	CLOTHES       = new KnowledgeBaseModule(knowledgeDir + "Veale's clothing line.txt", 1);  // 1 is the column number of the key value
 		CLOTHES       = new KnowledgeBaseModule(knowledgeDir + "clothing line.txt", 1);  // 1 is the column number of the key value
-		//		CREATIONS     = new KnowledgeBaseModule(knowledgeDir + "Veale's creations.txt", 0);
-		//		DOMAINS       = new KnowledgeBaseModule(knowledgeDir + "Veale's domains.txt", 0);
-		//		WORLDS        = new KnowledgeBaseModule(knowledgeDir + "Veale's fictional worlds.txt", 0);
+		
 		//VEHICLES      = new KnowledgeBaseModule(knowledgeDir + "Veale's vehicle fleet.txt", 1);  // 1 is the column number of the key value
 
 		VEHICLES      = new KnowledgeBaseModule(knowledgeDir + "vehicle fleet.txt", 1);  // 1 is the column number of the key value
-		//		WEAPONS	      = new KnowledgeBaseModule(knowledgeDir + "Veale's weapon arsenal.txt", 1);  // 1 is the column number of the key value
-		//		PLACES        = new KnowledgeBaseModule(knowledgeDir + "Veale's place elements.txt", 0);		
-		//		SUPERLATIVES  = new KnowledgeBaseModule(knowledgeDir + "superlatives.txt", 0);
-		//		COMPARATIVES  = new KnowledgeBaseModule(knowledgeDir + "comparatives.txt", 0);
-		//		ANTONYMS	  = new KnowledgeBaseModule(knowledgeDir + "antonyms.txt", 0);
-		//		PAST_PERFECTS = new KnowledgeBaseModule(knowledgeDir + "past perfects.txt", 0);
-		//		POS_QUALITIES = NOC.getInvertedField("Positive Talking Points");
-		//		NEG_QUALITIES = NOC.getInvertedField("Negative Talking Points");
-		//		ALL_QUALITIES = NOC.getInvertedField("Positive Talking Points");
-		//		ALL_QUALITIES = NOC.getInvertedField("Negative Talking Points", ALL_QUALITIES);
-		//		
-//		allPeople       = NOC.getAllFrames();
-//
-//		fictionalPeople = NOC.getAllKeysWithFieldValue("Fictive Status", "fictional");
-//		realPeople      = NOC.difference(allPeople, fictionalPeople);
-//
-//		men			    = NOC.getAllKeysWithFieldValue("Gender", "male");
-//		women			= NOC.getAllKeysWithFieldValue("Gender", "female");
-//
-//		allFields		= NOC.getFieldNames();
-//
-//		attributeFields = new Vector();
-//		attributeFields.add("Negative Talking Points");
-//		attributeFields.add("Positive Talking Points");	
+		
 
 
 
