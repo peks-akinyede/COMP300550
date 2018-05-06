ff6abb987aed59449ba031aab5db10125d02e44e
peks-akinyede <Olanipekun.Akinyede@UCDConnect.ie>
2018-05-06 13:40:39 +0100

    fixed sentence phrase mash up

--- tonyInterfaces/ChanceDeck.java
+++ tonyInterfaces/ChanceDeck.java
@@ -8,11 +8,11 @@ public class ChanceDeck extends CardDeck {
 
 		// Movement
 		cards.add(new Card(CHANCE_CARD,pan.MovementCards()+ " You've advanced to Go " ,ACT_GO_FORWARD,WorldBuilder.POS_GO));
-		cards.add(new Card(CHANCE_CARD,pan.MovementCards()+" If you pass Go collect " + UI.CURRENCY_SYMBOL + "200.",ACT_GO_FORWARD,WorldBuilder.POS_PALL_MALL));
-		cards.add(new Card(CHANCE_CARD,pan.MovementCards() + " if you pass Go collect " + UI.CURRENCY_SYMBOL + "200.",ACT_GO_FORWARD,WorldBuilder.POS_MARYLEBONE_STATION));
-		cards.add(new Card(CHANCE_CARD,pan.MovementCards()+ " If you pass Go collect " + UI.CURRENCY_SYMBOL + "200.",ACT_GO_FORWARD,WorldBuilder.POS_TRAFALGAR_SQ));
-		cards.add(new Card(CHANCE_CARD,pan.MovementCards() + " If you pass Go collect " + UI.CURRENCY_SYMBOL + "200.",ACT_GO_FORWARD,WorldBuilder.POS_MAYFAIR));
-		cards.add(new Card(CHANCE_CARD,pan.MovementCards() + " back three sqaures",ACT_MOVE,-3	));
+		cards.add(new Card(CHANCE_CARD,pan.MovementCards(),ACT_GO_FORWARD,WorldBuilder.POS_PALL_MALL));
+		cards.add(new Card(CHANCE_CARD,pan.MovementCards() ,ACT_GO_FORWARD,WorldBuilder.POS_MARYLEBONE_STATION));
+		cards.add(new Card(CHANCE_CARD,pan.MovementCards(),ACT_GO_FORWARD,WorldBuilder.POS_TRAFALGAR_SQ));
+		cards.add(new Card(CHANCE_CARD,pan.MovementCards() ,ACT_GO_FORWARD,WorldBuilder.POS_MAYFAIR));
+		cards.add(new Card(CHANCE_CARD,pan.MovementCards() + " back to",ACT_MOVE,-3	));
 		
 		// Fines
 		cards.add(new Card(CHANCE_CARD,pan.fineCard()+ " For each house pay " + UI.CURRENCY_SYMBOL + "25. For each hotel pay " + UI.CURRENCY_SYMBOL + "100.",ACT_PAY_HOUSES,new int[] {25,100}));
@@ -22,12 +22,12 @@ public class ChanceDeck extends CardDeck {
 		cards.add(new Card(CHANCE_CARD,"Speeding fine. " + UI.CURRENCY_SYMBOL + "15.",ACT_PAY,15));
 		
 		// Payments
-		cards.add(new Card(CHANCE_CARD,pan.RewardCard() + "Your building loan matures. Receive " + UI.CURRENCY_SYMBOL + "150.",ACT_RECEIVE,150));
-		cards.add(new Card(CHANCE_CARD,pan.RewardCard() + "You have won a crossword competition. Collect " + UI.CURRENCY_SYMBOL + "100.",ACT_RECEIVE,100));
-		cards.add(new Card(CHANCE_CARD,pan.RewardCard()+ "Bank pays you divided of " + UI.CURRENCY_SYMBOL + "50.",ACT_RECEIVE,50));
+		cards.add(new Card(CHANCE_CARD,pan.RewardCard() + " " + UI.CURRENCY_SYMBOL + "150.",ACT_RECEIVE,150));
+		cards.add(new Card(CHANCE_CARD,pan.RewardCard() +  UI.CURRENCY_SYMBOL + "100.",ACT_RECEIVE,100));
+		cards.add(new Card(CHANCE_CARD,pan.RewardCard()+ " " + UI.CURRENCY_SYMBOL + "50.",ACT_RECEIVE,50));
 
 		// Jail
-		cards.add(new Card(CHANCE_CARD,pan.goToJail() + "Go to jail. Move directly to jail. Do not pass Go. Do not collect " + UI.CURRENCY_SYMBOL + "200.",ACT_GOTO_JAIL));
+		cards.add(new Card(CHANCE_CARD,pan.goToJail() + " Go to jail. Move directly to jail. Do not pass Go. Do not collect " + UI.CURRENCY_SYMBOL + "200.",ACT_GOTO_JAIL));
 		cards.add(new Card(CHANCE_CARD,pan.GetOutOfJaillCard()+ "Get out of jail free.",ACT_GET_OUT_OF_JAIL));
 
 		shuffle();
