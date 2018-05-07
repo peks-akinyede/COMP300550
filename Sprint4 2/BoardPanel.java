b1b44b0ee3cbb75ab3a532980a105e181b945670
peks-akinyede <Olanipekun.Akinyede@UCDConnect.ie>
2018-05-07 14:30:05 +0100

    adjust coordinates a bit

--- Sprint4 2/BoardPanel.java
+++ Sprint4 2/BoardPanel.java
@@ -1,6 +1,8 @@
+package tonyInterfaces;
+
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
-
 import java.io.IOException;
 import java.awt.image.BufferedImage;
 import java.awt.*;
@@ -9,33 +11,33 @@ import java.awt.geom.*;
 class BoardPanel extends JPanel {
 	
 	private static final long serialVersionUID = 1L;
-	private static final int FRAME_WIDTH = 750;    // must be even
-	private static final int FRAME_HEIGHT = 750;
+	private static final int FRAME_WIDTH = 700;    // must be even
+	private static final int FRAME_HEIGHT = 700;
 	private static final int TOKEN_RADIUS = 8;   // must be even
 	private static final Color[] PLAYER_COLORS = {Color.RED,Color.BLUE,Color.YELLOW,Color.GREEN,Color.MAGENTA,Color.WHITE};
-	private static final String[] TOKEN_NAME = {"red","blue","yellow","green","magenta","white"};
+	public static final String[] TOKEN_NAME = {"red","blue","yellow","green","magenta","white"};
 	private static final float[] PLAYER_OFFSET = {0, 0.01f, 0.02f, 0.03f, 0.04f, 0.05f};
-	private static final float[][] CORNER_FROM = { {710, 730}, {5, 700}, {40,5}, {730, 40},};
-	private static final float[][] CORNER_TO = {{60, 730}, {5, 70}, {700,5}, {730, 700}};
+	private static final float[][] CORNER_FROM = { {650, 630}, {10, 620}, {40,5}, {630, 40},};
+	private static final float[][] CORNER_TO = {{60, 630}, {0, 70}, {600,5}, {630, 600}};
 	private static final int[] JAIL_COORDS = {60,700};
 	
 	private Players players;	
 	private BufferedImage boardImage;
-	private int[][][] squareCoords = new int [Board.NUM_SQUARES][Players.MAX_NUM_PLAYERS][2];
+	static int[][][] squareCoords = new int [WorldBuilder.NUM_SQUARES][Players.MAX_NUM_PLAYERS][2];
 	private int[][] jailCoords = new int [Players.MAX_NUM_PLAYERS][2];
 	
 	BoardPanel (Players players) {
 		this.players = players;
 		setPreferredSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
+		
 		setBackground(Color.WHITE);
 		try {
-//			boardImage = ImageIO.read(new File("board.jpg"));
-			boardImage = ImageIO.read(this.getClass().getResource("board.jpg"));
+			boardImage = ImageIO.read(this.getClass().getResource("board.png"));
 		} catch (IOException ex) {
 			System.out.println("Could not find the image file " + ex.toString());
 		}
-		int sideLength = Board.NUM_SQUARES/4;
- 		for (int s=0; s<Board.NUM_SQUARES; s++) {
+		int sideLength = WorldBuilder.NUM_SQUARES/4;
+ 		for (int s=0; s<WorldBuilder.NUM_SQUARES; s++) {
 			for (int p=0; p<Players.MAX_NUM_PLAYERS; p++) {
 		        	int side = (int) s / sideLength;
 		        	float offset = (float) (s % sideLength) / sideLength + PLAYER_OFFSET[p];
@@ -53,7 +55,7 @@ class BoardPanel extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
-        g2.drawImage(boardImage, 0, 0, FRAME_WIDTH, FRAME_HEIGHT, this);
+        g2.drawImage(boardImage, 0, 0,this.getWidth(),this.getHeight(),this);
         for (int p=0; p<players.numPlayers(); p++) {
  	        int[] coords;
        		if (players.get(p).isInJail()) {
